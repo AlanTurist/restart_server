dcb44b5e5226f408a33b0c904a324fee188ca4b0
AlanTurist <59668425+AlanTurist@users.noreply.github.com>
2020-08-14 08:58:43 +0300

    Create countdown_def.py

---
+++ countdown_def.py
@@ -0,0 +1,11 @@
+import time
+
+def countdown(t):
+	while t > 0:
+		m, s = divmod(t, 60)
+		h, m = divmod(m, 60)
+		time_left = str(h).zfill(2) + ":" + str(m).zfill(2) + ":" + str(s).zfill(2)
+		print(time_left + "\r", end="")
+		time.sleep(1)
+		t -= 1
+	print()
