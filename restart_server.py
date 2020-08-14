990c5c43a7e6a723933311298d1d9e6df3730dc3
AlanTurist <59668425+AlanTurist@users.noreply.github.com>
2020-08-14 08:58:51 +0300

    Create restart_server.py

---
+++ restart_server.py
@@ -0,0 +1,8 @@
+import countdown_def
+import restart_def
+
+print("\n\n\t~ Script για επανεκκίνηση του Server ~\n\n@Author: Γεώργιος Κολιού, IT - Manager Casa Cook Rhodes")
+
+t = int(input("\n\n\tΣε πόσα δευτερόλεπτα να γίνει επανεκκίνηση; :"))
+countdown_def.countdown(t)
+restart_def.restart()
